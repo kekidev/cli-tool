e4be292814d9e6a6f09dc45db0e95df2e67a1296
keki <baejaehwan1125@gmail.com>
2021-07-21 14:53:41 +0900

    pushing automatically with bash script

--- discord.sh
+++ discord.sh
@@ -1,6 +1,11 @@
-cd /Users/keki/dev 
+#!/bin/bash 
+
+path=/User/keki/dev
+projectName=discord-bot
+
+cd $path
 git clone https://github.com/kekidev/d.py_cogs_template discord-bot
-cd discord-bot 
+cd $projectName
 python3 -m venv venv
 source ./venv/bin/activate
 pip install -r requirements.txt 
